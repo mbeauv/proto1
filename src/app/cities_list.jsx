aee8d0598a72b4d803700aeab417c4c781b55e32
Martin Beauvais <mbeauv@gmail.com>
2017-05-26 09:19:11 -0400

    Added empty component for CityList

---
+++ src/app/cities_list.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CitiesList extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Cities</h1>
+      </div>
+    );
+  }
+
+}
+
+export default CitiesList;
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import CitiesList from './cities_list.jsx';
+
+var cities = [
+  {
+    name: "Montreal",
+    citizens: 12131
+  },
+  {
+    name: "Ottawa",
+    citizens: 4421
+  }
+];
+
 class App extends React.Component {
   render () {
-    return <p> Hello React Once Again And Again!</p>;
+    return (
+      <div>
+        <CitiesList />
+      </div>
+    );
   }
 }
 
